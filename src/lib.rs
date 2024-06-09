1e3f58b82c23acbcf69d09ff15bd37ab040e15de
Dasun Pubudumal <pubudumald@gmail.com>
2024-06-09 19:51:17 +0100

    Working on the lexer

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,94 @@
-use std::fs;
+use std::collections::VecDeque;
+use std::fmt::Display;
 use std::io::stdin;
+use std::{fs, vec};
 
 use scanner::Scanner;
-mod scanner;
 mod error_handler;
+mod scanner;
+
+#[derive(Debug)]
+pub struct Literal {}
+
+impl Display for Literal {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
 
 /// Different types of tokens
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum TokenType {
     // Single-character tokens.
-    LeftParen, RightParen, LeftBrace, RightBrace,
-    Comma, Dot, Minus, Plus, SemiColon, Slash, Eof,
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    SemiColon,
+    Slash,
+    Eof,
     Start,
 
     // One or two character tokens
-    Bang, BangEqual, Equal, EqualEqual,
-    Greater, GreaterEqual,
-    Less, LessEqual,
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
 
     // Literals
-    Identifier, String, Number,
+    Identifier,
+    String,
+    Number,
 
     // Keywords
-    And, Class, Else, False, Fun, For, If, Nil, Or,
-    Print, Return, Super, This, True, Var, While,
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
 }
 
+/// Token type for lexical analysis
 pub struct Token {
-    kind: TokenType
+    // Type of the token. I have used `kind` here as `type` is a reserved word in Rust
+    pub kind: TokenType,
+    // For example, in the statement int x = 10;, there would be four lexemes: `"int"`, `"x"`, `"="`, and `"10"`.
+    pub lexeme: String,
+    // For many tokens, this would be None, but for literals this needs to be set as its own type (e.g. `String` for strings)
+    pub literal: Option<Literal>,
+    // Line number of the
+    pub line: usize,
+}
+
+impl Token {
+    pub fn to_string(&self) -> String {
+        format!("{:?} {:?} {:?}", self.kind, self.lexeme, self.literal)
+    }
 }
 
 /// Run the source code file
 pub fn run_file(file_path: &str) -> impl Iterator<Item = Token> + '_ {
     let content = fs::read_to_string(file_path).expect("Invalid file path");
-    run(&content).collect::<Vec<_>>().into_iter()
+    run(&content).collect::<Vec<Token>>().into_iter()
 }
 
 /// Run REPL input
@@ -50,12 +105,5 @@ pub fn run_prompt() {
 /// Run either the source code or REPL line
 fn run(line: &str) -> impl Iterator<Item = Token> + '_ {
     let mut scanner = Scanner::new(line);
-    std::iter::from_fn(move || {
-        let next_token = scanner.next_token();
-        if next_token.kind != TokenType::Eof {
-            Some(next_token)
-        } else {
-            None
-        }
-    })
+    scanner.scan_tokens().collect::<Vec<Token>>().into_iter()
 }
--- src/scanner.rs
+++ src/scanner.rs
@@ -5,7 +5,8 @@ use crate::TokenType;
 pub struct Scanner<'a> {
     pub code: &'a str,
     pub current_token: Option<Token>,
-    pub previous_token: Option<Token>
+    pub previous_token: Option<Token>,
+    pub current_line: usize,
 }
 
 /// We need to guarantee that the reference `code` we provide into `new()` lives throughout the Scanner instance.
@@ -16,14 +17,40 @@ impl<'a> Scanner<'a> {
         Scanner {
             code,
             current_token: Some(Token {
-                kind: TokenType::Start
+                kind: TokenType::Start,
+                lexeme: String::from(""),
+                literal: None,
+                line: 0,
             }),
-            previous_token: None
+            previous_token: None,
+            current_line: 0,
         }
     }
 
-    /// Returns the next token
-    pub fn next_token(&mut self) -> Token {
+    /// Returns an iterator that contains tokens of type `Token`.
+    /// Chained token is the EOF token that makes parsing a little easier.
+    pub fn scan_tokens(&'a mut self) -> impl Iterator<Item = Token> + 'a {
+        let current_line = self.current_line;
+        std::iter::from_fn(move || {
+            // This still moves `self` into the closure.
+            // Figure out a way around this? `Rc` and `RefCell` might be good candiates to solve this.
+            let next_token = self.next_token();
+            if next_token.kind != TokenType::Eof {
+                Some(next_token)
+            } else {
+                None
+            }
+        })
+        .chain(std::iter::once(Token {
+            kind: TokenType::Eof,
+            lexeme: String::from(""),
+            literal: None,
+            line: current_line,
+        }))
+    }
+
+    /// Returns the next token. This is a private function only used by `scan_tokens()` function
+    fn next_token(&mut self) -> Token {
         todo!("Yet to be implemented")
     }
 }
